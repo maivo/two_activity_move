87f0705e43c3d3ad47725968a747c4995d6312c6
phonger <vupt@yahoo.com>
2015-05-24 22:33:11 -0600

    01_add

---
+++ app/src/main/java/pee/actionbar/app/SecondActivity.java
@@ -0,0 +1,64 @@
+package pee.actionbar.app;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SecondActivity extends Activity {
+
+    private static final String TAG = "SecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id ==android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void handleGoMainActivity(){
+        Log.i(TAG, "inside handleGoMainActivity");
+
+        //Intent intent = new Intent(this, MainActivity.class);
+
+        // calling an activity using <intent-filter> action name
+        //  Intent intent = new Intent("com.hmkcode.android.ANOTHER_ACTIVITY");
+
+        //startActivity(intent);
+        finish();
+
+    }
+
+}
